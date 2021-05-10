9055a9671cda5447c0bd2d8dd0073fe45eb724c5
samuelvertello <83984319+samuelvertello@users.noreply.github.com>
2021-05-10 17:31:46 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        Terminal terminal = new Terminal();
+        
+        terminal.gerenciarEstacionamento();
+    }
+    
+}
